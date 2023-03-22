f256deb4e44d2672920334ce5d54aeed1afb33a2
ggandur <grgandur@gmail.com>
2023-03-22 15:20:37 -0300

    Initial commit IOA NBA App

--- MobileApp/ContentView.swift
+++ MobileApp/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var myArrayOfPlayers: [PlayerType] = PlayerData.players
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                Text("NBA Players")
+                    .bold()
+                    .font(.system(size: 48))
+                Text("2022 Season")
+                    .bold()
+                    .font(.system(size: 24))
+                Divider()
+                PlayerListView(players: myArrayOfPlayers)
+            }
         }
-        .padding()
     }
 }
 
@@ -23,4 +29,50 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct PlayerContainerView: View {
+    var playerName: String
+    var playerImage: String
+
+    var body: some View {
+        VStack {
+            ZStack {
+                Color(uiColor: .lightGray)
+                    .cornerRadius(12)
+                HStack {
+                    Image(playerImage)
+                        .resizable()
+                        .scaledToFit()
+                        .aspectRatio(contentMode: .fit)
+                        .padding(EdgeInsets(top: 10, leading: 10, bottom: 0, trailing: 10))
+                    Text(playerName)
+                        .bold()
+                        .font(.system(size: 24))
+                    Spacer()
+                }
+            }
+        }
+        .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+        .frame(width: .infinity, height: 64)
+    }
+}
+
+struct PlayerListView: View {
+    var players: [PlayerType]
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                ForEach(players, id: \.self) { player in
+                    PlayerContainerView(playerName: player.name, playerImage: player.image)
+                }
+            }
+        }
+    }
+}
+
+struct PlayerType: Hashable {
+    var name: String
+    var image: String
 }
---
+++ MobileApp/PlayerData.swift
@@ -0,0 +1,25 @@
+//
+//  PlayerData.swift
+//  MobileApp
+//
+//  Created by Gabriel Gandur on 22/03/23.
+//
+
+import Foundation
+
+struct PlayerData {
+    static let players: [PlayerType] = [
+        PlayerType(name: "LeBron James", image: "lebron"),
+        PlayerType(name: "Kevin Durant", image: "durant"),
+        PlayerType(name: "Luka Doncic", image: "doncic"),
+        PlayerType(name: "LeBron James", image: "lebron"),
+        PlayerType(name: "Kevin Durant", image: "durant"),
+        PlayerType(name: "Luka Doncic", image: "doncic"),
+        PlayerType(name: "LeBron James", image: "lebron"),
+        PlayerType(name: "Kevin Durant", image: "durant"),
+        PlayerType(name: "Luka Doncic", image: "doncic"),
+        PlayerType(name: "LeBron James", image: "lebron"),
+        PlayerType(name: "Kevin Durant", image: "durant"),
+        PlayerType(name: "Luka Doncic", image: "doncic"),
+    ]
+}
